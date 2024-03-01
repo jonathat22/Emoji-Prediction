7c3c0a1af23841884184f50bc31b4ceedab8fca1
Jonathan Taylor <jonathat@andrew.cmu.edu>
2024-03-01 13:56:23 -0800

    changed predict function to output an array

--- bento_service.py
+++ bento_service.py
@@ -11,28 +11,31 @@ def create_bento_service_torch(bento_name):
     Create a Bento service for a PyTorch model.
     """
     # Load model
-    model = bentoml.pytorch.get("emoji_bentoml_model:latest").to_runner()
+    model = bentoml.pytorch.get(bento_name).to_runner()
 
     # Create the service
     service = bentoml.Service(bento_name + "_service", runners=[model])
 
     return model, service
 
-model, service = create_bento_service_torch("emoji_bentoml_service")
+model, service = create_bento_service_torch("emoji_bentoml_model")
 
 
-@service.api(input=Text(), output=int)
-@torch.no_grad()
-def predict(sentence):
+@service.api(input=Text(), output=NumpyNdarray())
+def predict(sentence_str) -> np.ndarray:
     """
     Returns emoji classification prediction for a sample test sentence
     """
     max_length  = 10
     embedding_layer, word2idx, idx2word = torch_pretrained_embedding()
-    sentence = np.array([sentence])
-    sentence = sentences_to_indices(sentence, word2idx, max_length)
-    sentence = torch.from_numpy(sentence)
-    model.init_local()
-    predictions = model.forward.run(sentence, predict=True)
-    _, max_prediction = torch.max(predictions, dim=1)
-    return max_prediction.numpy()[0]
+    sentence_str = np.array([sentence_str])
+    sentence_str = sentences_to_indices(sentence_str, word2idx, max_length)
+    sentence_str = torch.from_numpy(sentence_str)
+    with torch.no_grad():
+        predictions = model.forward.run(sentence_str, predict=True)
+        _, max_prediction = torch.max(predictions, dim=1)
+    result = max_prediction.numpy()
+
+    return result
+
+#print(predict("I could not be more confused"))
