45ac1a9d23d7ef192ef24845614137dce2c082bb
Jonathan Taylor <jonathat@andrew.cmu.edu>
2024-03-18 19:44:31 -0700

    UI now doesn't do predictions via api

--- ui.py
+++ ui.py
@@ -5,9 +5,9 @@ import torch
 import emoji
 import unicodeit
 from src.map_pred_to_emoji import *
+from predict import *
 
-# "http://localhost:3000/predict"
-API_ENDPOINT = "http://localhost:3000/predict"
+
 
 st.set_page_config(layout="wide")
 
@@ -38,32 +38,15 @@ st.write("""
     predict the most relevant emoji for an input sentence. \U0001F92F
 """)
 
-def predict(sentence):
-    """
-    A function that sends a prediction request to the API and returns the predicted emoji
-    """
-    response = requests.post(API_ENDPOINT, headers={"content-type": "text/plain"}, data=sentence)
-
-    if response.status_code == 200:
-        return response.text
-    else:
-        raise Exception("Status: {}".format(response.status_code))
-
 
 def main():
     sentence = st.text_input("Type in your sentence here")
     if len(sentence.split()) > 0:
         if len(sentence.split()) <= 10: # max sequence length is 10
-            prediction = predict(sentence)
-            st.write(prediction)
-            if len(prediction) == 3:
-                prediction = int(prediction[1]) # output is [2] or [26], for example, so the if elif statements are extracting the numbers
-            elif len(prediction) == 4:
-                prediction = int(prediction[1:3])
+            prediction = predict(sentence)[0]
             emoji_map = build_emoji_df(filepath="Data/full_emoji.csv")
-            emoji_name = emoji_map['name'].iloc[prediction]
-            st.title(emoji.emojize(f":{emoji_name}:"))
-            #st.success(f"Your predicted emoji is {prediction:.3f}")
+            emoji = emoji_map['emoji'].iloc[prediction]
+            st.title(emoji)
         else:
             st.write("Sequence is too long. It must have 10 or fewer words.")
             st.write(len(sentence.split()))
