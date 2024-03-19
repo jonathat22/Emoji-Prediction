4093edc5e3fdd2599124b541792639c7596bf369
Jonathan Taylor <jonathat@andrew.cmu.edu>
2024-03-18 23:50:30 -0700

    caching

--- ui.py
+++ ui.py
@@ -44,13 +44,13 @@ st.write("""
 ####################################### Model Inference #############################################
 
 
-@st.cache_data
+@st.cache_data()
 def load_embeddings():
     embedding_layer, word2idx, idx2word = torch_pretrained_embedding()
     return embedding_layer, word2idx, idx2word
 
 
-@st.cache_resource
+@st.cache_resource()
 def load_model(_embedding_layer):
     model = GRUNet(input_dim=10, 
                 hidden_dim=128,
