c71f18827a5d3454243e6b48c0621d885723feaf
Jonathan Taylor <jonathat@andrew.cmu.edu>
2024-03-25 13:25:29 -0700

    gave more descriptive variable names

--- src/map_pred_to_emoji.py
+++ src/map_pred_to_emoji.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import streamlit as st
 
 
-def convert_unicode_column(emoji_df):
+def convert_unicode_column(emoji_names_unicode_data):
     """
     reformats emoji unicodes to be Python friendly
     Example: U+1F601 -> "\\U0001F601"
@@ -16,12 +16,12 @@ def convert_unicode_column(emoji_df):
     Returns:
     emoji_df -- emoji_df with adjusted unicode column
     """
-    emoji_df['unicode'] = "\n" + emoji_df['unicode'].str.replace("+", "000").astype(str)
-    emoji_df['name'] = emoji_df['name'].str.replace(" ", "_").astype(str)
-    return emoji_df
+    emoji_names_unicode_data['unicode'] = "\n" + emoji_names_unicode_data['unicode'].str.replace("+", "000").astype(str)
+    emoji_names_unicode_data['name'] = emoji_names_unicode_data['name'].str.replace(" ", "_").astype(str)
+    return emoji_names_unicode_data
 
 
-def subset_emoji_df(full_emoji):
+def subset_emoji_emoji_names_unicode_data(full_emoji):
     """
     returns subset of emoji_df with the 30 most popular emojis
     according to Emojipedia.com
@@ -68,13 +68,13 @@ def subset_emoji_df(full_emoji):
                             'disappointed face']
 
     most_popular_emojis = popular_smileys + popular_gestures + other_popular_emojis
-    data = full_emoji['name'].isin(most_popular_emojis)
-    data = full_emoji[data].reset_index()
-    return data
+    subset_with_popular_emojis = full_emoji['name'].isin(most_popular_emojis)
+    subset_with_popular_emojis = full_emoji[subset_with_popular_emojis].reset_index()
+    return subset_with_popular_emojis
 
 
 @st.cache_data
-def build_emoji_df(filepath):
+def build_emoji_df(emoji_names_unicode_data_path):
     """
     loads and processes full emoji dataset.
     calls convert_unicode_column() and subset_emoji_df()
@@ -86,13 +86,13 @@ def build_emoji_df(filepath):
     emoji_df -- dataframe containing index, emoji and unicode columns
     """
 
-    full_emoji = pd.read_csv(filepath)
-    emoji_df = subset_emoji_df(full_emoji)
-    emoji_df = convert_unicode_column(emoji_df)
-    cols_to_drop = ["index", "#", "Apple", 
+    full_emoji = pd.read_csv(emoji_names_unicode_data_path)
+    most_popular_emojis_df = subset_emoji_emoji_names_unicode_data(full_emoji)
+    most_popular_emojis_df = convert_unicode_column(most_popular_emojis_df)
+    unnessecary_columns = ["index", "#", "Apple", 
                     "Google", "Facebook", "Windows", 
                     "Twitter", "JoyPixels", "Samsung", 
                     "Gmail", "SoftBank", "DoCoMo", "KDDI"]
 
-    emoji_df = emoji_df.drop(cols_to_drop, axis=1)
-    return emoji_df
+    most_popular_emojis_df = most_popular_emojis_df.drop(unnessecary_columns, axis=1)
+    return most_popular_emojis_df
